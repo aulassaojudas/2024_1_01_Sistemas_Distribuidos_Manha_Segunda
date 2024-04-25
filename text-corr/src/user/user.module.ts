331a20de3a29be5efe007dfd93bc545fb7945010
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2024-04-25 07:09:41 -0300

    Criando o service e contoller para user

---
+++ text-corr/src/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { User } from './entity/user.entity';
+import { Filiacao } from './entity/filiacao.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Filiacao])],
+  providers: [UserService],
+  controllers: [UserController],
+})
+export class UserModule {}
