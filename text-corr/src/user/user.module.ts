03cecfea5514127823a1b17c1cca19bf4306a800
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2024-04-29 18:29:24 -0300

    Add files via upload

---
+++ text-corr/src/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { User } from './entity/user.entity';
+import { Filiacao } from './entity/filiacao.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+import { FiliacaoService } from './service/filiacao.service';
+import { FiliacaoController } from './controller/filiacao.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Filiacao])],
+  providers: [UserService, FiliacaoService],
+  controllers: [UserController, FiliacaoController],
+})
+export class UserModule {}
