d8d533c5d91f98fc2ef8ee058df3e7fb2f5216f7
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2024-05-20 09:02:11 -0300

    Delete text-corr directory

--- text-corr/src/auth/guard/jwt-auth.guard.strategy.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JwtAuthGuardStrategy } from './jwt-auth.guard.strategy';
-
-describe('JwtAuthGuardStrategy', () => {
-  it('should be defined', () => {
-    expect(new JwtAuthGuardStrategy()).toBeDefined();
-  });
-});
--- text-corr/src/user/dto/filicacao.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FilicacaoDto } from './filicacao.dto';
-
-describe('FilicacaoDto', () => {
-  it('should be defined', () => {
-    expect(new FilicacaoDto()).toBeDefined();
-  });
-});
